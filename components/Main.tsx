6cf46130ea29e6ff93884932fb824cbfb650e351
Dusousas <dusousadeveloper@gmail.com>
2025-04-09 18:39:28 -0300

    redes s

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { FaFacebookSquare, FaInstagramSquare, FaWhatsappSquare } from 'react-icons/fa';
 
 export default function Main() {
     return (
         <>
-            <section id='home' className='mainBG relative top-0  flex items-center'>
+            <section id='home' className='mainBG relative top-0 flex items-center'>
                 <div className='maxW'>
                     <h5 className='text-white tracking-widest italic text-center text-xl'>Tattoo & Piercing</h5>
                     <h1 className='text-center tracking-widest text-5xl italic font-semibold uppercase mt-6 lg:text-8xl'>Monkey’s</h1>
@@ -13,6 +14,11 @@ export default function Main() {
                         <a className='text-white uppercase border px-6 py-3' href="">Saiba mais</a>
                     </div>
 
+                    <article className='flex gap-1 absolute bottom-5 right-5'>
+                        <a className='text-3xl' href=""><FaInstagramSquare /></a>
+                        <a className='text-3xl' href=""><FaFacebookSquare /></a>
+                        <a className='text-3xl' href=""><FaWhatsappSquare /></a>
+                    </article>
                 </div>
             </section>
         </>
